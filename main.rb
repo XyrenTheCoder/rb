cbb865f9cd949fd543196e5b651962a3df190ec2
e <69575053+thatOneArchUser@users.noreply.github.com>
2022-08-23 15:47:05 +0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -2,22 +2,23 @@ require "discordrb"
 
 rb_bot = Discordrb::Commands::CommandBot.new token: <token>, client_id: <id>, prefix: "<prefix>"
 
-# commands
+#commands
 rb_bot.command :sum, min_args: 2 do |event, *args|
-    event.respond (args.collect(&:to_f).inject(&:+)).next_float()
+  event.respond args.collect(&:to_f).inject(&:+).to_s
 end
 
 rb_bot.command :subtract, min_args: 2 do |event, *args|
-    event.respond args.collect(&:to_f).inject(&:-)
+  event.respond args.collect(&:to_f).inject(&:-).to_s
 end
 
 rb_bot.command :multiply, min_args: 2 do |event, *args|
-    event.respond args.collect(&:to_f).inject(&:*)
+  event.respond args.collect(&:to_f).inject(&:*).to_s
 end
 
 rb_bot.command :divide, min_args: 2 do |event, *args|
-    event.respond args.collect(&:to_f).inject(&:/)
+  event.respond args.collect(&:to_f).inject(&:/).to_s
 end
+
 
 rb_bot.run
 rb_bot.join
