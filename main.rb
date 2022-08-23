d7ca748aa0573cd33e2a303bb6cac09f9d788162
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-08-23 20:18:37 +0800

    Update main.rb

--- main.rb
+++ main.rb
@@ -4,19 +4,19 @@ rb_bot = Discordrb::Commands::CommandBot.new token: <token>, client_id: <id>, pr
 
 # commands
 rb_bot.command :sum, min_args: 2 do |event, *args|
-    event.respond args.collect(&:to_i).collect(&:+)
+    event.respond args.collect(&:to_f).inject(&:+)
 end
 
 rb_bot.command :subtract, min_args: 2 do |event, *args|
-    event.respond args.collect(&:to_i).collect(&:-)
+    event.respond args.collect(&:to_f).inject(&:-)
 end
 
 rb_bot.command :multiply, min_args: 2 do |event, *args|
-    event.respond args.collect(&:to_i).collect(&:*)
+    event.respond args.collect(&:to_f).inject(&:*)
 end
 
 rb_bot.command :divide, min_args: 2 do |event, *args|
-    event.respond args.collect(&:to_i).collect(&:/)
+    event.respond args.collect(&:to_f).inject(&:/)
 end
 
 rb_bot.run
