b5cfa95dcbcdae8ae6a983aef60ec8d66372e9c8
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-08-23 20:39:21 +0800

    Update main.rb

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ rb_bot = Discordrb::Commands::CommandBot.new token: <token>, client_id: <id>, pr
 
 # commands
 rb_bot.command :sum, min_args: 2 do |event, *args|
-    event.respond args.collect(&:to_f).inject(&:+)
+    event.respond (args.collect(&:to_f).inject(&:+)).next_float()
 end
 
 rb_bot.command :subtract, min_args: 2 do |event, *args|
