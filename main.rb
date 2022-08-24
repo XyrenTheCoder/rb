28c610a9fd2087a9106cf9f1a7906f3448cf9146
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-08-24 17:39:47 +0800

    Update main.rb

--- main.rb
+++ main.rb
@@ -21,7 +21,8 @@ rb_bot.command :div, min_args: 2 do |event, *args| #divide
 end
 
 rb_bot.command :sqrt do |event, arg| #square root
-  event.respond Math.sqrt(arg.to_f).to_s
+  event.respond CMath.sqrt(arg.join(" ").to_c).to_s
+  #event.respond Math.sqrt(arg.to_f).to_s
 end
 
 rb_bot.command :abs do |event, arg| #absolute value
