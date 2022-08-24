f126e2b418839f6a5e8374d5f389f228c0f7f060
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-08-24 18:03:52 +0800

    Update main.rb

--- main.rb
+++ main.rb
@@ -21,32 +21,38 @@ rb_bot.command :div, min_args: 2 do |event, *args| #divide
 end
 
 # complex
-rb_bot.command :sqrt do |event, *args| #square root
+rb_bot.command :sqrt do |event, *arg| #square root
   event.respond CMath.sqrt(arg.join(" ").to_c).to_s
   #event.respond Math.sqrt(arg.to_f).to_s
 end
 
-rb_bot.command :cbrt do |event, *args| #cube root
+rb_bot.command :cbrt do |event, *arg| #cube root
   event.respond CMath.cbrt(arg.join(" ").to_c).to_s
 end
 
-rb_bot.command :abs do |event, arg| #absolute value
+rb_bot.command :abs do |event, *arg| #absolute value
   event.respond arg.to_c.abs().to_s
 end
 
-rb_bot.command :sin do |event, arg| #sine
+rb_bot.command :sin do |event, *arg| #sine
   event.respond CMath.sin(arg.join(" ").to_c).to_s
 end
 
-rb_bot.command :cos do |event, arg| #cosine
+rb_bot.command :cos do |event, *arg| #cosine
   event.respond CMath.cos(arg.join(" ").to_c).to_s
 end
 
-rb_bot.command :tan do |event, arg| #tangent
+rb_bot.command :tan do |event, *arg| #tangent
   event.respond CMath.tan(arg.join(" ").to_c).to_s
 end
 
+rb_bot.command :sinh do |event, *arg| #hyperbolic sine
+  event.respond CMath.sinh(arg.join(" ").to_c).to_s
+end
 
+rb_bot.command :cosh do |event, *arg| #hyperbolic cosine
+  event.respond CMath.cosh(arg.join(" ").to_c).to_s
+end
 
 rb_bot.run
 rb_bot.join
