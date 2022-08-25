93d653f82d0087cd3a178e8ae5d0e0cd45abb2a8
thatOneArchUser <69575053+thatOneArchUser@users.noreply.github.com>
2022-08-25 12:53:47 +0300

    Update q.py

--- q.py
+++ q.py
@@ -4,6 +4,10 @@ import numpy as np
 from sympy import *
 from math import e, tau, pi
 
+for i in string.ascii_lowercase:
+  if i != "x" and i != "e":
+    globals()[i] = Symbol(i)
+
 def derivative(expr):
   x = Symbol("x")
   try: expr = expr.replace("^", "**")
@@ -19,29 +23,29 @@ def integral(expr):
   out = str(integrate(eval(expr), x))
   return print(str(out).replace("**", "^").replace("*", "\\*") + " + C")
 
-# def graph(expr):
-#   expr = expr.replace("^", "**")
-#   h = dir(sympy)
-#   obj = ["+", "-", "/", "*", "(", ")", ".", ",", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "=", "cum"]
-#   for i in obj: c = c.replace(i, " ")
-#   for i in c.split():
-#     if i not in h: raise BadArgument
+#def graph(expr):
+  #expr = expr.replace("^", "**")
+  #h = dir(sympy)
+  #obj = ["+", "-", "/", "*", "(", ")", ".", ",", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "=", "cum"]
+  #for i in obj: c = c.replace(i, " ")
+  #for i in c.split():
+    #if i not in h: raise BadArgument
   
-#   xpoints = list(np.arrange(-20, 20, 0.1))
-#   ypoints = []
-#   for i in xpoints:
-#     ypoints.append(eval(expr.replace("x", str(i))))
-#   plt.axhline(color="black")
-#   plt.plot(xpoints, ypoints, "c", label=f"y={expr.replace("**", "^")}")
-#   try: os.remove("graph.png")
-#   except FileNotFoundError: pass
-#   plt.savefig("graph.png")
-#   plt.close()
+  #xpoints = list(np.arrange(-20, 20, 0.1))
+  #ypoints = []
+  #for i in xpoints:
+    #ypoints.append(eval(expr.replace("x", str(i))))
+  #plt.axhline(color="black")
+  #plt.plot(xpoints, ypoints, "c", label=f"y={expr.replace("**", "^")}")
+  #try: os.remove("graph.png")
+  #except FileNotFoundError: pass
+  #plt.savefig("graph.png")
+  #plt.close()
 
 def encode(text):
     arr = []
     for i in text:
-        if i not in list(string.letters + string.digits): return f"Invalid character at position {text.index(i)}"
+        if i not in list(string.ascii_letters + string.digits): return f"Invalid character at position {text.index(i)}"
         if i == " ": arr.append("*")
         elif i.isupper(): arr.append(f"{(int(hex(ord(i)), 16) - int('0x40', 16))*'+'}@")
         elif i.islower(): arr.append(f"{(int(hex(ord(i)), 16) - int('0x60', 16))*'+'}#")
@@ -69,10 +73,10 @@ def decode(text):
         elif i == "*": arr.append(" ")
         elif i == "!": value = 0
         elif i == "&": arr.append(str(value))
-    return print(''.join(arr))
+    return print(''.join(arr) if len(''.join(arr)) > 0 else '_ _')
   
 if sys.argv[1] == "d": derivative(sys.argv[2])
 elif sys.argv[1] == "i": integral(sys.argv[2])
 elif sys.argv[1] == "g": graph(sys.argv[2])
 elif sys.argv[1] == "e": encode(sys.argv[2])
-elif sys.argv[1] == "de": decode(sys.argv[2]
+elif sys.argv[1] == "de": decode(sys.argv[2])
