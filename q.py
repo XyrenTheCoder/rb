57464000b31b7eceb906e6ceba92180da252f6ce
thatOneArchUser <69575053+thatOneArchUser@users.noreply.github.com>
2022-08-25 12:34:12 +0300

    Update q.py

--- q.py
+++ q.py
@@ -19,24 +19,24 @@ def integral(expr):
   out = str(integrate(eval(expr), x))
   return print(str(out).replace("**", "^").replace("*", "\\*") + " + C")
 
-def graph(expr):
-  expr = expr.replace("^", "**")
-  h = dir(sympy)
-  obj = ["+", "-", "/", "*", "(", ")", ".", ",", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "=", "cum"]
-  for i in obj: c = c.replace(i, " ")
-  for i in c.split():
-    if i not in h: raise BadArgument
+# def graph(expr):
+#   expr = expr.replace("^", "**")
+#   h = dir(sympy)
+#   obj = ["+", "-", "/", "*", "(", ")", ".", ",", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "=", "cum"]
+#   for i in obj: c = c.replace(i, " ")
+#   for i in c.split():
+#     if i not in h: raise BadArgument
   
-  xpoints = list(np.arrange(-20, 20, 0.1))
-  ypoints = []
-  for i in xpoints:
-    ypoints.append(eval(expr.replace("x", str(i))))
-  plt.axhline(color="black")
-  plt.plot(xpoints, ypoints, "c", label=f"y={expr.replace("**", "^")}")
-  try: os.remove("graph.png")
-  except FileNotFoundError: pass
-  plt.savefig("graph.png")
-  plt.close()
+#   xpoints = list(np.arrange(-20, 20, 0.1))
+#   ypoints = []
+#   for i in xpoints:
+#     ypoints.append(eval(expr.replace("x", str(i))))
+#   plt.axhline(color="black")
+#   plt.plot(xpoints, ypoints, "c", label=f"y={expr.replace("**", "^")}")
+#   try: os.remove("graph.png")
+#   except FileNotFoundError: pass
+#   plt.savefig("graph.png")
+#   plt.close()
 
 def encode(text):
     arr = []
